528b619234a683b68b34c630ec47466dd1bc0b03
Rodrigo <rodrigograndem@gmail.com>
2024-05-16 17:41:42 +0200

    #4 Eliminado metodo sobrante Price.getCharge

--- src/main/java/ubu/gii/Price.java
+++ src/main/java/ubu/gii/Price.java
@@ -2,8 +2,5 @@ package ubu.gii;
 
 public abstract class Price {
 	public abstract int getPriceCode();
-	public double getCharge (Movie movie,int daysRented) {
-		return getCharge(daysRented);
-	}
 	public abstract double getCharge(int daysRented);
 }
